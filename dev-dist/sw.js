8fded9cc9d3e0a930759c2b1158e5e94b5efc1b3
jaybaheliya <jaybaheliya@gmail.com>
2026-03-17 22:06:35 +0530

    ss

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6n5f3b8qfbk"
+    "revision": "0.1oi6dej3vfs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
